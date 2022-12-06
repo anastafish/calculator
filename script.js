b4722ae8de2e1e3eba9e30776a2ce9c714903bd1
anastafish <anastafish24@gmail.com>
2022-12-06 23:59:25 +0300

    all the operations work but only equation with two operands for now

--- script.js
+++ script.js
@@ -1,61 +1,67 @@
 const buttons = document.querySelectorAll('.btn')
 const display = document.querySelector('#display-header')
+const operatorBtn = document.querySelectorAll('.operator-btn')
 btnClear = document.querySelector('.btn-clear')
 btnEqual = document.querySelector('.btn-equal')
 let num1 = 0;
 let num2 = 0;
 let operator; 
+let currentNumber = 0;
+
+
+btnEqual.addEventListener('click', () =>{
+    num2 = currentNumber;
+    if (num1 && num2 && operator){
+        console.log(operate(Number(num1),Number(num2),operator))
+    }
+} )
+
 buttons.forEach(btn =>{
     btn.addEventListener('click', () => {
-        display.textContent += btn.textContent
-        for (let i = 0; i < display.textContent.length; i++){
-        num1 = display.textContent[0]
-        operator = display.textContent[1]
-        num2 = display.textContent[2]        
+        display.textContent += btn.textContent;
+        if (num1 === 0){
+        if (btn.className == "btn"){
+        currentNumber += btn.textContent;
+    }
+    }
+        else {
+            if (btn.className == "btn"){
+            currentNumber += btn.textContent;            
         }
-        console.log(num1)
-        console.log(num2)
-        console.log(operator)
-        // if (num1 && num2 && operator){
-        //     console.log(operate(Number(num1), Number(num2), operator))
-        // }
+            
+        }        
+        
+
     })
 })
 
-btnClear.addEventListener('click', () => display.textContent = '')
+operatorBtn.forEach(operatorBtn =>{
+    operatorBtn.addEventListener('click', () =>{
+        num1 = currentNumber;
+        operator = operatorBtn.textContent;
+        currentNumber = 0;
+})})
+
+btnClear.addEventListener('click', () => {
+    display.textContent = ''
+    num1 = 0;
+    num2 = 0;
+    currentNumber = 0;
+})
 
 
 function operate(a, b, operation){
     if (operation === '-'){
-        subtraction(a,b)
+        return a - b;
     }
-    else if (operation === '+'){
-        addition(a,b)
+    else if (operation === "+"){
+        return a + b;
     }
     else if (operation === '×'){
-        multyply(a,b)
+        return a * b;
     }
     else if (operation === '÷'){
-        devide(a,b)
+        return a / b;
     }
-}
-
-console.log(operate(Number(1), Number(5), '+'))
-
-
-function addition(a, b){
-    return a + b;
-}
-
-function subtraction(a, b){
-    return a - b;
-}
-
-function devide(a, b){
-    return a / b;
-}
-
-function multyply(a, b){
-    return a * b;
 }
 
